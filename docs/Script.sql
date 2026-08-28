9093d2cfe692ecf6545d53e7d98d24765273346e
spyy-dahh <boikanyodimpho123@gmail.com>
2026-08-28 13:53:44 +0200

    Update Script.sql
    Inserted data into every table.

--- docs/Script.sql
+++ docs/Script.sql
@@ -87,3 +87,51 @@ CREATE TABLE Result (
     FOREIGN KEY (EnrolmentID)
     REFERENCES Enrolment(EnrolmentID)
 )
+INSERT INTO Users (FirstName, LastName, Email, Password, PhoneNumber, Role)
+VALUES
+('Sarah','Mokoena','sarahm@raceday.co.za','Pass123!','0825652391','Organiser'),
+('Thabo','Ndlovu','thabondlovu432@raceday.co.za','Pass123!','0832459045','Organiser'),
+('Lebo','Khumalo','lebok@gmail.com','Pass123!','0694203267','Participant'),
+('Ayesha','Patel','ayeshapatel22@gmail.com','Pass123!','0718517393','Participant')
+
+INSERT INTO Event
+(OrganiserID, Name, Description, EventDate, Location, Distance, EventType)
+VALUES
+(1,'Soweto Marathon','Annual road marathon through Soweto','2027-11-07','Johannesburg',42.20,'Run'),
+
+(1,'Cape Town Cycle Tour','Iconic cycling event around Cape Town','2027-03-14','Cape Town',109.00,'Cycle'),
+
+(2,'Durban Charity Walk','Community fundraising walk','2027-06-20','Durban',10.00,'Walk')
+
+INSERT INTO Category(EventID, CategoryName, CategoryType)
+VALUES
+(1,'Senior','Age'),
+(1,'Veteran','Age'),
+(1,'42.2km','Distance'),
+(2,'Open','Age'),
+(2,'109km','Distance'),
+(3,'Open','Age'),
+(3,'10km','Distance')
+
+INSERT INTO Route(EventID, RouteName, Distance, RouteDescription)
+VALUES
+(1,'Soweto Marathon Route',42.20,
+'Official Soweto Marathon route.'),
+
+(2,'Cape Town Cycle Tour Route',109.00,
+'Scenic coastal cycling route.'),
+
+(3,'Durban Charity Walk Route',10.00,
+'Family-friendly fundraising walk.');
+
+INSERT INTO Enrolment (UserID, EventID, CategoryID)
+VALUES
+(3,1,3),   -- Lebo enters 42.2km Marathon
+(3,3,7),   -- Lebo enters Durban Walk
+(4,2,5),   -- Ayesha enters Cycle Tour
+(4,3,7);   -- Ayesha enters Walk
+
+INSERT INTO Result (EnrolmentID, FinishTime, Position)
+VALUES
+(1,'03:28:15',128),
+(3,'04:15:42',341)
